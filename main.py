b5e3b282c9c5bdbfc1011b0d3ce65f4fde7d1b5e
lehakot-create <lehakot85@inbox.ru>
2023-05-24 20:49:34 +0400

    добавил хосты в CORS

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from datetime import date
 from typing import List, Dict
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from data_base.db_query import DB
 
 app = FastAPI()
 db = DB()
+
+origins = [
+    'http://localhost:3000',
+    'http://0.0.0.0:3000',
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Flight(BaseModel):
