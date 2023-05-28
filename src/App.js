2044fb98ef4d2652818f0922c1225964b2b41382
DeKatel <de.katel@yandex.ru>
2023-05-28 18:53:45 +0300

    new Tab

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import AppView from "./components/AppView/AppView";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { setAllFlight } from "./storage/slises/dataSlise";
+import { setAllFlight, setTabs } from "./storage/slises/dataSlise";
 
 function App() {
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(setAllFlight())
+    dispatch(setAllFlight());
+    dispatch(setTabs("Сезонность спроса"));
   }, [])
 
   return (
